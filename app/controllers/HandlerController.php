c39dbf3bda3edeb2f5738ca5d0beec3fda75734b
marktristan <mt.victorio@dot.ph>
2013-11-20 17:00:00 +0800

    Domain update done

--- app/controllers/HandlerController.php
+++ app/controllers/HandlerController.php
@@ -71,10 +71,10 @@ class HandlerController extends EppController {
   private function domainUpdate($request)
   {
     $data = json_decode($request)->data;
-    print_r($data);
-    //$response = Epp::$epp->request(Domain::update($data));
-    return Domain::update($data);
-    //return Epp::result($response, __FUNCTION__);
+    
+    $response = Epp::$epp->request(Domain::update($data));
+    
+    return Epp::result($response, __FUNCTION__);
   }
   
   private function contactCheck($request)
