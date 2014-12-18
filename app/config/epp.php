07fdd2eaa29729bdcd6fc798e7e90ca227be3c03
Mark Tristan Victorio <mt.victorio@dot.ph>
2014-12-18 16:09:06 +0800

    Update epp.php
    Updated epp config

--- app/config/epp.php
+++ app/config/epp.php
@@ -2,86 +2,14 @@
 
 return array(
 
-  'registry' => 'coccaProduction', // The default registry to connect to epp
+  'registry' => 'testRegistry', // The default registry to connect to epp
 
-  'cocca' => array(
-    'host' => '10.1.11.83',
+  'testRegistry' => array(
+    'host' => 'hostname',
     'port' => 700,
     'timeout' => 10,
     'ssl' => true,
-    'pw' => 'Qwerty123'
-  ),
-
-  'coccaStaging' => array(
-    'host' => '10.1.11.67',
-    'port' => 700,
-    'timeout' => 10,
-    'ssl' => true,
-    'pw' => 'Qwerty123'
-  ),
-
-  'coccaSandbox' => array(
-    'host' => '203.119.4.158',
-    'port' => 700,
-    'timeout' => 10,
-    'ssl' => true,
-    'pw' => 'Qwerty123'
-  ),
-
-  'coccaNewBeta' => array(
-    'host' => '10.1.11.203',
-    'port' => 700,
-    'timeout' => 10,
-    'ssl' => true,
-    'pw' => 'Qwerty123'
-  ),
-
-  'coccaNewStaging' => array(
-    'host' => 'cocca2.dotphstaging.ph',
-    'port' => 700,
-    'timeout' => 10,
-    'ssl' => true,
-    'pw' => 'Qwerty123'
-  ),
-
-  'coccaNewSandbox' => array(
-    'host' => 'cocca2.sandbox.dot.ph',
-    'port' => 700,
-    'timeout' => 10,
-    'ssl' => true,
-    'pw' => 'Qwerty123'
-  ),
-
-  'coccaProduction' => array(
-    'host' => 'cocca.dot.ph',
-    'port' => 700,
-    'timeout' => 10,
-    'ssl' => true,
-    'pw' => 'Qwerty123'
-  ),
-
-  'fred' => array(
-    'host' => '10.1.11.111',
-    'port' => 700,
-    'timeout' => 10,
-    'ssl' => true,
-    'pw' => 'Qwerty123'
-  ),
-
-  'keysystems' => array(
-    'host' => 'wi-ote2.rrpproxy.net',
-    'port' => 700,
-    'timeout' => 10,
-    'ssl' => true,
-    'pw' => 'oc34nside'
-  ),
-
-  'webnic' => array(
-    'host' => 'oteweb.webnic.cc',
-    'port' => 700,
-    'timeout' => 60,
-    'ssl' => true,
-    'pw' => 'd0tcom@'
+    'pw' => 'password'
   ),
 
   'version' => '1.0',
